2abc6ff75e94ccbfa738e3b0b0f309d3f3f8a5a3
segin <98380527+segin-GH@users.noreply.github.com>
2023-01-14 16:05:27 +0530

    Update getrichquick.sh

--- getrichquick.sh
+++ getrichquick.sh
@@ -1,15 +1,9 @@
 #! /usr/bin/zsh
 
 getrich=$(($RANDOM % 14))
-
 echo "What is your name"
-
 read name
-
 echo "what is your age"
-
 read age
-
 echo "Hello, $name, your are $age years old."
-
 echo "you are going to get rich at $(($age + $getrich))"
