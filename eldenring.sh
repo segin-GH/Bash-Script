42a9ce3ddf4c95b69d15e86bde3430d44369a9bd
segin-GH <segin.bytes@gmail.com>
2023-01-16 09:29:20 +0530

    .

--- eldenring.sh
+++ eldenring.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/zsh
 
-beastNum=$(($RANDOM % 2))
+beastNum=$(( $RANDOM % 10 ))
 
 echo "Enter a num btw 0/1"
 read num
-
 
 if [[ $beastNum == $num ]]; then
     echo "you win"
 else
     echo "you lost"
+    exit 1
 fi
