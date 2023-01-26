fea5c95330ba9a9588b70cf0ede18864eddf94f7
segin-GH <segin.bytes@gmail.com>
2023-01-26 11:02:13 +0530

    .

--- CRUD/post-branch.sh
+++ CRUD/post-branch.sh
@@ -1,5 +1,17 @@
 #! /usr/bin/zsh
 
+func_read_from_file(){
+    while read line; do
+        branch_name=$(echo $line | awk '{print $1}')
+        if git show-branch $branch_name; then
+            echo "Branch $branch_name is available"
+        else
+            echo "Branch $branch_name is not available"
+            sed -i "/$branch_name/d" version.txt
+        fi
+    done < version.txt
+}
+
 
 if [ -e version.txt ]; then
     echo "Version.txt file already Exists"
@@ -21,9 +33,6 @@ if [ -z "$line_found" ]; then
     printf "$cur_branch $ver_num \n" > version.txt.tmp
     cat version.txt >> version.txt.tmp
     mv version.txt.tmp version.txt
-
-else
-    
 fi
 
-cur_branch=$(git rev-parse --abbrev-ref HEAD)
+func_read_from_file
